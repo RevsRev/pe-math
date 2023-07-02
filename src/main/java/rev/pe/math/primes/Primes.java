ab706fccc982a4a1948567da9d5b7d6e46c5239c
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-07-02 20:20:26 +0100

    Added beginnings of modular arithmetic module

--- src/main/java/rev/pe/math/primes/Primes.java
+++ src/main/java/rev/pe/math/primes/Primes.java
@@ -8,4 +8,5 @@ public class Primes
     private static final long PRIME_CACHE_SIZE = 10000;
     private static LinkedHashSet<Long> primeCache = SieveOfEratosthenes.sieveOfEratosthenes(PRIME_CACHE_SIZE);
 
+
 }
---
+++ src/test/java/rev/pe/math/modular/TestMod.java
@@ -0,0 +1,24 @@
+package rev.pe.math.modular;
+
+import lombok.extern.slf4j.Slf4j;
+import modular.Mod;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TestMod
+{
+    @Test
+    public void testPow() {
+        Assertions.assertEquals(0, Mod.pow(5,3,25));
+        Assertions.assertEquals(0, Mod.pow(6, 4, 48));
+        Assertions.assertEquals(1, Mod.pow(2,0,6));
+        Assertions.assertEquals(1, Mod.pow(5, 4, 3));
+        Assertions.assertEquals(1, Mod.pow(7, 4, 5));
+        Assertions.assertEquals(1, Mod.pow(9, 6, 13));
+        Assertions.assertEquals(1, Mod.pow(25, 30, 31));
+        Assertions.assertEquals(4, Mod.pow(2, 5, 7));
+        Assertions.assertEquals(1, Mod.pow(145, 7918, 7919));
+    }
+
+}
