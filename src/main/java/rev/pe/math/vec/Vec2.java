49cf69fd2a75deeca9a8f320ed031c721635eca9
Eddie Revell <eddie.revell@yahoo.co.uk>
2023-07-07 15:35:31 +0100

    Changes to Vec2

--- src/main/java/rev/pe/math/vec/Vec2.java
+++ src/main/java/rev/pe/math/vec/Vec2.java
@@ -10,7 +10,33 @@ public class Vec2
         this.y = y;
     }
 
+    public Vec2(Vec2 v) {
+        x = v.x;
+        y = v.y;
+    }
     public double dot(Vec2 v) {
         return x*v.x + y*v.y;
+    }
+
+    public void plus(Vec2 v) {
+        x += v.x;
+        y += v.y;
+    }
+
+    public void mult(double a) {
+        x*=a;
+        y*=a;
+    }
+
+    private static final double EPSILON = 0.001;
+    public static Vec2 normalize(Vec2 p) {
+        double x = p.x;
+        double y = p.y;
+        double magnitude = Math.sqrt(x*x + y*y);
+
+        if (magnitude < EPSILON) {
+            return new Vec2(0,0);
+        }
+        return new Vec2(x/magnitude, y/magnitude);
     }
 }
